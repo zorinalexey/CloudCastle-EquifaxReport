961463aefb813bc093ae3eeedc12d84138047c4c
Зорин Алексей <zorinalexey59292@gmail.com>
2023-02-13 17:47:14 +0400

    add post-install-script

--- src/Bin/PostInstall.php
+++ src/Bin/PostInstall.php
@@ -6,9 +6,15 @@ class PostInstall
 {
     public static function init()
     {
-        $shellScript = __DIR__.DIRECTORY_SEPARATOR.'postInstallCmd.sh';
-        exec("chmod 777 ".$shellScript);
-        exec($shellScript, $out, $code);
+        $command = 'dir='.__DIR__.DIRECTORY_SEPARATOR.'uidgen_src'.PHP_EOL;
+        $command .= 'cd $dir'.PHP_EOL;
+        $command .= 'make'.PHP_EOL;
+        $command .= 'cd ../'.PHP_EOL;
+        $command .= 'cp $dir/uidgen $(pwd)/uidgen'.PHP_EOL;
+        $command .= 'rm $(pwd)/uidgen_src/uidgen'. PHP_EOL;
+        $command .= 'rm $(pwd)/uidgen_src/*.o'.PHP_EOL;
+        $command .= 'chmod 777 $(pwd)/uidgen'.PHP_EOL;
+        exec($command, $out, $code);
         return !$code;
     }
 }
